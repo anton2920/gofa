50b3cac966f4c7370298b623e36ac674b6224537
anton2920 <anton2920@gmail.com>
2024-05-30 21:54:22 +0300

    Added all <errno.h> constants and implemented 'Strerror()'

--- syscall/error.go
+++ syscall/error.go
@@ -7,20 +7,6 @@ type Error struct {
 	Errno int
 }
 
-const (
-	/* From <errno.h>. */
-	ENOENT      = 2      /* No such file or directory */
-	EINTR       = 4      /* Interrupted system call */
-	EEXIST      = 17     /* File exists */
-	EPIPE       = 32     /* Broken pipe */
-	EAGAIN      = 35     /* Resource temporarily unavailable */
-	EWOULDBLOCK = EAGAIN /* Operation would block */
-	EINPROGRESS = 36     /* Operation now in progress */
-	EOPNOTSUPP  = 45     /* Operation not supported */
-	ECONNRESET  = 54     /* Connection reset by peer */
-	ENOSYS      = 78     /* Function not implemented */
-)
-
 func NewError(name string, errno uintptr) error {
 	if errno == 0 {
 		return nil
@@ -28,7 +14,6 @@ func NewError(name string, errno uintptr) error {
 	return Error{Name: name, Errno: int(errno)}
 }
 
-/* TODO(anton2920): add strerror(). */
 func (e Error) Error() string {
-	return fmt.Sprintf("%s failed with code %d", e.Name, e.Errno)
+	return fmt.Sprintf("%s failed with code %d (%s)", e.Name, e.Errno, Strerror(e.Errno))
 }
