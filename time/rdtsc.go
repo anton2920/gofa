60f5aeca10c98568da899057936b2af7e707a628
anton2920 <anton2920@gmail.com>
2024-07-02 17:40:10 +0300

    Renamed 'Freq' -> 'Hz'

--- time/rdtsc.go
+++ time/rdtsc.go
@@ -2,18 +2,18 @@ package time
 
 type Cycles uint64
 
-var CpuFreq Cycles
+var CpuHz Cycles
 
 func init() {
-	const millisecondsToWait = 1
-	const osFreq = int64(1 * NsecPerSec)
+	const osHz = int64(1 * NsecPerSec)
+	const millisecondsToWait = 10
 
 	cpuStart := RDTSC()
 	osStart := UnixNsec()
 
 	osEnd := UnixNsec()
 	osElapsed := int64(0)
-	osWaitTime := osFreq * millisecondsToWait / 1000
+	osWaitTime := osHz * millisecondsToWait / MsecPerSec
 	for osElapsed < osWaitTime {
 		osEnd = UnixNsec()
 		osElapsed = osEnd - osStart
@@ -21,15 +21,15 @@ func init() {
 
 	cpuEnd := RDTSC()
 	cpuElapsed := int64(cpuEnd - cpuStart)
-	CpuFreq = Cycles(osFreq * cpuElapsed / osElapsed)
+	CpuHz = Cycles(cpuElapsed * osHz / osElapsed)
 }
 
 func RDTSC() Cycles
 
 func (c Cycles) ToNsec() int64 {
-	return int64(c * NsecPerSec / CpuFreq)
+	return int64(c * NsecPerSec / CpuHz)
 }
 
 func (c Cycles) ToUsec() int64 {
-	return int64(c * UsecPerSec / CpuFreq)
+	return int64(c * UsecPerSec / CpuHz)
 }
