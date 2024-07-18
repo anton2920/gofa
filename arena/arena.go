72a9f76c10fd9686f970e36f5cbc7ee90cbb15ef
anton2920 <anton2920@gmail.com>
2024-07-18 15:33:21 +0300

    arena: removed incorrect implementation

--- arena/arena.go
+++ arena/arena.go
@@ -1,21 +1,11 @@
 package arena
 
 type Arena struct {
-	Buffer []byte
-	Used   int
 }
 
 func (a *Arena) NewSlice(n int) []byte {
-	if a.Used+n >= cap(a.Buffer) {
-		buffer := make([]byte, max(a.Used+n*2, cap(a.Buffer)*2))
-		copy(buffer, a.Buffer)
-		a.Buffer = buffer
-	}
-	ret := a.Buffer[a.Used : a.Used+n]
-	a.Used += n
-	return ret
+	return make([]byte, n)
 }
 
 func (a *Arena) Reset() {
-	a.Used = 0
 }
