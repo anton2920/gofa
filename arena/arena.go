332a1cbae409f916fe04eace149d8ba88227ac1e
anton2920 <anton2920@gmail.com>
2024-07-18 16:59:19 +0300

    arena: returned old implementation, but without extra copy (possibly incorrect)

--- arena/arena.go
+++ arena/arena.go
@@ -1,11 +1,20 @@
 package arena
 
 type Arena struct {
+	Buffer []byte
+	Used   int
 }
 
 func (a *Arena) NewSlice(n int) []byte {
-	return make([]byte, n)
+	if a.Used+n >= cap(a.Buffer) {
+		a.Buffer = make([]byte, max(a.Used+n*2, cap(a.Buffer)*2))
+		a.Used = 0
+	}
+	ret := a.Buffer[a.Used : a.Used+n]
+	a.Used += n
+	return ret
 }
 
 func (a *Arena) Reset() {
+	a.Used = 0
 }
