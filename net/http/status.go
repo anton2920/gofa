47829241d28851d07708f708f35b133ee3d5a255
anton2920 <anton2920@gmail.com>
2025-06-02 13:32:53 +0300

    net/http: added 'StatusCreated'

--- net/http/status.go
+++ net/http/status.go
@@ -3,23 +3,25 @@ package http
 type Status int
 
 const (
-	StatusOK                    Status = 200
-	StatusSeeOther                     = 303
-	StatusBadRequest                   = 400
-	StatusUnauthorized                 = 401
-	StatusForbidden                    = 403
-	StatusNotFound                     = 404
-	StatusMethodNotAllowed             = 405
-	StatusRequestTimeout               = 408
-	StatusConflict                     = 409
-	StatusRequestEntityTooLarge        = 413
-	StatusInternalServerError          = 500
-	StatusServiceUnavailable           = 503
+	StatusOK                    = Status(200)
+	StatusCreated               = 201
+	StatusSeeOther              = 303
+	StatusBadRequest            = 400
+	StatusUnauthorized          = 401
+	StatusForbidden             = 403
+	StatusNotFound              = 404
+	StatusMethodNotAllowed      = 405
+	StatusRequestTimeout        = 408
+	StatusConflict              = 409
+	StatusRequestEntityTooLarge = 413
+	StatusInternalServerError   = 500
+	StatusServiceUnavailable    = 503
 )
 
 var Status2String = [...]string{
 	0:                           "200",
 	StatusOK:                    "200",
+	StatusCreated:               "201",
 	StatusSeeOther:              "303",
 	StatusBadRequest:            "400",
 	StatusUnauthorized:          "401",
@@ -36,6 +38,7 @@ var Status2String = [...]string{
 var Status2Reason = [...]string{
 	0:                           "OK",
 	StatusOK:                    "OK",
+	StatusCreated:               "Created",
 	StatusSeeOther:              "See Other",
 	StatusBadRequest:            "Bad Request",
 	StatusUnauthorized:          "Unauthorized",
