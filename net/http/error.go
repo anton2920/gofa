3965e8c5b6fdb2c3a12c39d0980b3f2291e09cbf
anton2920 <anton2920@gmail.com>
2025-07-02 15:11:38 +0300

    net/http: added ability to change display error message for client and server errors

--- net/http/error.go
+++ net/http/error.go
@@ -7,52 +7,57 @@ import (
 )
 
 type Error struct {
-	StatusCode     Status
-	DisplayMessage string
-	LogError       error
+	StatusCode          Status
+	DisplayErrorMessage string
+	LogError            error
 }
 
 var (
-	NoSpaceLeft    = Error{StatusCode: StatusRequestEntityTooLarge, DisplayMessage: "no space left in the buffer", LogError: errors.New("no space left in the buffer")}
-	TooManyClients = Error{StatusCode: StatusServiceUnavailable, DisplayMessage: "too many clients", LogError: errors.New("too many clients")}
+	ClientDisplayErrorMessage = "whoops... Something went wrong. Please reload this page or try again later"
+	ServerDisplayErrorMessage = "whoops... Something went wrong. Please try again later"
+)
+
+var (
+	NoSpaceLeft    = Error{StatusCode: StatusRequestEntityTooLarge, DisplayErrorMessage: "no space left in the buffer", LogError: errors.New("no space left in the buffer")}
+	TooManyClients = Error{StatusCode: StatusServiceUnavailable, DisplayErrorMessage: "too many clients", LogError: errors.New("too many clients")}
 )
 
 func BadRequest(format string, args ...interface{}) Error {
 	message := fmt.Sprintf(format, args...)
-	return Error{StatusCode: StatusBadRequest, DisplayMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
+	return Error{StatusCode: StatusBadRequest, DisplayErrorMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
 }
 
 func Unauthorized(format string, args ...interface{}) Error {
 	message := fmt.Sprintf(format, args...)
-	return Error{StatusCode: StatusUnauthorized, DisplayMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
+	return Error{StatusCode: StatusUnauthorized, DisplayErrorMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
 }
 
 func Forbidden(format string, args ...interface{}) Error {
 	message := fmt.Sprintf(format, args...)
-	return Error{StatusCode: StatusForbidden, DisplayMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
+	return Error{StatusCode: StatusForbidden, DisplayErrorMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
 }
 
 func NotFound(format string, args ...interface{}) Error {
 	message := fmt.Sprintf(format, args...)
-	return Error{StatusCode: StatusNotFound, DisplayMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
+	return Error{StatusCode: StatusNotFound, DisplayErrorMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
 }
 
 func Conflict(format string, args ...interface{}) Error {
 	message := fmt.Sprintf(format, args...)
-	return Error{StatusCode: StatusConflict, DisplayMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
+	return Error{StatusCode: StatusConflict, DisplayErrorMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
 }
 
 func RequestEntityTooLarge(format string, args ...interface{}) Error {
 	message := fmt.Sprintf(format, args...)
-	return Error{StatusCode: StatusRequestEntityTooLarge, DisplayMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
+	return Error{StatusCode: StatusRequestEntityTooLarge, DisplayErrorMessage: message, LogError: errors.WrapWithTrace(errors.New(message), 2)}
 }
 
 func ClientError(err error) Error {
-	return Error{StatusCode: StatusBadRequest, DisplayMessage: "whoops... Something went wrong. Please reload this page or try again later", LogError: errors.WrapWithTrace(err, 2)}
+	return Error{StatusCode: StatusBadRequest, DisplayErrorMessage: ClientDisplayErrorMessage, LogError: errors.WrapWithTrace(err, 2)}
 }
 
 func ServerError(err error) Error {
-	return Error{StatusCode: StatusInternalServerError, DisplayMessage: "whoops... Something went wrong. Please try again later", LogError: errors.WrapWithTrace(err, 2)}
+	return Error{StatusCode: StatusInternalServerError, DisplayErrorMessage: ServerDisplayErrorMessage, LogError: errors.WrapWithTrace(err, 2)}
 }
 
 func (err Error) Error() string {
