94a00a4960d34edd147e1f32bc028f09936b0639
anton2920 <anton2920@gmail.com>
2025-09-11 14:56:13 +0300

    time: added Timezone

---
+++ time/timezone.go
@@ -0,0 +1,37 @@
+package time
+
+type Timezone int32
+
+const (
+	TimezoneNone = Timezone(iota - 13)
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	TimezoneUTC
+	_
+	_
+	TimezoneMSK
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	_
+	TimezoneCount
+)
