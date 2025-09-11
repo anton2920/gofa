ba4a2dc54490ced42334f03c4926a6296653e918
anton2920 <anton2920@gmail.com>
2025-09-11 15:00:27 +0300

    time: added 'Timezone2String'

--- time/timezone.go
+++ time/timezone.go
@@ -35,3 +35,35 @@ const (
 	_
 	TimezoneCount
 )
+
+var Timezone2String = map[int]string{
+	-12: "UTC-12",
+	-11: "UTC-11",
+	-10: "UTC-10",
+	-9:  "UTC-9",
+	-8:  "UTC-8",
+	-7:  "UTC-7",
+	-6:  "UTC-6",
+	-5:  "UTC-5",
+	-4:  "UTC-4",
+	-3:  "UTC-3",
+	-2:  "UTC-2",
+	-1:  "UTC-1",
+	0:   "UTC+0",
+	1:   "UTC+1",
+	2:   "UTC+2",
+	3:   "UTC+3",
+	4:   "UTC+4",
+	5:   "UTC+5",
+	6:   "UTC+6",
+	7:   "UTC+7",
+	8:   "UTC+8",
+	9:   "UTC+9",
+	10:  "UTC+10",
+	11:  "UTC+11",
+	12:  "UTC+12",
+	13:  "UTC+13",
+	14:  "UTC+14",
+	15:  "UTC+15",
+	16:  "UTC+16",
+}
