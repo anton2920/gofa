2991ee153c6cab25c29dfd32d62f3f91c1b4f742
anton2920 <anton2920@gmail.com>
2025-09-30 17:06:33 +0300

    strings: fixed 'And' function

--- strings/and.go
+++ strings/and.go
@@ -1,10 +1,11 @@
 package strings
 
 func And(vs ...string) string {
-	for i := 1; i < len(vs); i++ {
+	var i int
+	for i = 1; i < len(vs); i++ {
 		if len(vs[i]) == 0 {
-			return vs[i-1]
+			break
 		}
 	}
-	return vs[0]
+	return vs[i-1]
 }
