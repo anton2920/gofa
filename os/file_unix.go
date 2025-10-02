4a96568e08762e52699db0d1e4bfee270d5f6c1a
anton2920 <anton2920@gmail.com>
2025-10-02 19:03:13 +0300

    os: instrumented functions with trace calls

--- os/file_unix.go
+++ os/file_unix.go
@@ -3,16 +3,29 @@
 
 package os
 
-import "github.com/anton2920/gofa/os/unix"
+import (
+	"github.com/anton2920/gofa/os/unix"
+	"github.com/anton2920/gofa/trace"
+)
 
 func Close(handle Handle) error {
 	return unix.Close(int32(handle))
 }
 
 func Read(handle Handle, buf []byte) (int64, error) {
-	return unix.Read(int32(handle), buf)
+	t := trace.Begin("")
+
+	n, err := unix.Read(int32(handle), buf)
+
+	trace.End(t)
+	return n, err
 }
 
 func Write(handle Handle, buf []byte) (int64, error) {
-	return unix.Write(int32(handle), buf)
+	t := trace.Begin("")
+
+	n, err := unix.Write(int32(handle), buf)
+
+	trace.End(t)
+	return n, err
 }
