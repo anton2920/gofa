4f412b9181d8144f7fdd3a9e5d5796391c9374ac
anton2920 <anton2920@gmail.com>
2025-10-02 19:03:24 +0300

    alloc: instrumented functions with trace calls

--- alloc/arena.go
+++ alloc/arena.go
@@ -3,6 +3,7 @@ package alloc
 import (
 	"github.com/anton2920/gofa/bytes"
 	"github.com/anton2920/gofa/ints"
+	"github.com/anton2920/gofa/trace"
 )
 
 type Arena struct {
@@ -11,24 +12,36 @@ type Arena struct {
 }
 
 func (a *Arena) NewSlice(n int) []byte {
+	t := trace.Begin("")
+
 	if a.Used+n >= cap(a.Buffer) {
 		a.Buffer = make([]byte, ints.Max(a.Used+n*2, cap(a.Buffer)*2))
 		a.Used = 0
 	}
 	ret := a.Buffer[a.Used : a.Used+n]
 	a.Used += n
+
+	trace.End(t)
 	return ret
 }
 
 func (a *Arena) Copy(bs []byte) []byte {
+	t := trace.Begin("")
+
 	ret := a.NewSlice(len(bs))
 	copy(ret, bs)
+
+	trace.End(t)
 	return ret
 }
 
 func (a *Arena) CopyString(s string) string {
+	t := trace.Begin("")
+
 	ret := a.NewSlice(len(s))
 	copy(ret, s)
+
+	trace.End(t)
 	return bytes.AsString(ret)
 }
 
