c407dec1148c31f8b360515826b29d93b165175b
anton2920 <anton2920@gmail.com>
2025-10-04 00:44:19 +0300

    os: WIP: added temporary implementation of 'Open' function using 'syscall' package

--- os/file_unix.go
+++ os/file_unix.go
@@ -5,8 +5,15 @@ package os
 
 import (
 	"github.com/anton2920/gofa/os/unix"
+	"github.com/anton2920/gofa/syscall"
 	"github.com/anton2920/gofa/trace"
 )
+
+/* TODO(anton2920): rewrite using 'unix' package. */
+func Open(path string) (Handle, error) {
+	f, err := syscall.Open(path, syscall.O_RDONLY, 0)
+	return Handle(f), err
+}
 
 func Close(handle Handle) error {
 	return unix.Close(int32(handle))
