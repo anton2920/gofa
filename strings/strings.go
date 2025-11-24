6a6fb997ae95da8de168ccde10b0bfda13fd1aa9
anton2920 <anton2920@gmail.com>
2025-11-24 17:30:37 +0300

    strings: added 'nosplit' directive

--- strings/strings.go
+++ strings/strings.go
@@ -5,10 +5,12 @@ import (
 	"unsafe"
 )
 
+//go:nosplit
 func AsBytes(s string) []byte {
 	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: (*reflect.StringHeader)(unsafe.Pointer(&s)).Data, Len: len(s), Cap: len(s)}))
 }
 
+//go:nosplit
 func Data(s string) *byte {
 	return (*byte)(unsafe.Pointer((*reflect.StringHeader)(unsafe.Pointer(&s)).Data))
 }
