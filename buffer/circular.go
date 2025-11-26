fb7506e35baa9f8d8ecf34c5dc84fa5c61b76247
anton2920 <anton2920@gmail.com>
2025-11-26 18:32:33 +0300

    buffer: changed 'NewCircular' to return value instead of pointer

--- buffer/circular.go
+++ buffer/circular.go
@@ -10,12 +10,12 @@ import (
 )
 
 type Circular struct {
-	Buf  []byte
-	Head int
-	Tail int
+	Buffer []byte
+	Head   int
+	Tail   int
 }
 
-func NewCircular(size int) (*Circular, error) {
+func NewCircular(size int) (Circular, error) {
 	var c Circular
 
 	const pageSize = 4096
@@ -24,42 +24,42 @@ func NewCircular(size int) (*Circular, error) {
 	/* NOTE(anton2920): first argument is SHM_ANON, cannot have that as a variable as Go's checkptr doesn't like it. */
 	fd, err := syscall.ShmOpen2(*(*string)(unsafe.Pointer(&reflect.StringHeader{Data: 1, Len: 8})), syscall.O_RDWR, 0, 0, syscall.NULL)
 	if err != nil {
-		return nil, err
+		return c, err
 	}
 
 	defer syscall.Close(fd)
 
 	if err := syscall.Ftruncate(fd, int64(size)); err != nil {
-		return nil, err
+		return c, err
 	}
 
 	buffer, err := syscall.Mmap(nil, 2*uint64(size), syscall.PROT_NONE, syscall.MAP_PRIVATE|syscall.MAP_ANON, -1, 0)
 	if err != nil {
-		return nil, err
+		return c, err
 	}
 
 	if _, err := syscall.Mmap(buffer, uint64(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED|syscall.MAP_FIXED, fd, 0); err != nil {
-		return nil, err
+		return c, err
 	}
 	if _, err := syscall.Mmap(pointers.Add(buffer, size), uint64(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED|syscall.MAP_FIXED, fd, 0); err != nil {
-		return nil, err
+		return c, err
 	}
-	c.Buf = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: uintptr(buffer), Len: 2 * size, Cap: 2 * size}))
+	c.Buffer = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: uintptr(buffer), Len: 2 * size, Cap: 2 * size}))
 
 	/* NOTE(anton2920): sanity checks. */
-	c.Buf[0] = '\x00'
-	c.Buf[size-1] = '\x00'
-	c.Buf[size] = '\x00'
-	c.Buf[2*size-1] = '\x00'
+	c.Buffer[0] = '\x00'
+	c.Buffer[size-1] = '\x00'
+	c.Buffer[size] = '\x00'
+	c.Buffer[2*size-1] = '\x00'
 
-	return &c, nil
+	return c, nil
 }
 
 func (c *Circular) Consume(n int) {
 	c.Head += n
-	if c.Head > len(c.Buf)/2 {
-		c.Head -= len(c.Buf) / 2
-		c.Tail -= len(c.Buf) / 2
+	if c.Head > len(c.Buffer)/2 {
+		c.Head -= len(c.Buffer) / 2
+		c.Tail -= len(c.Buffer) / 2
 	}
 }
 
@@ -68,11 +68,11 @@ func (c *Circular) Produce(n int) {
 }
 
 func (c *Circular) RemainingSlice() []byte {
-	return c.Buf[c.Tail : c.Head+len(c.Buf)/2]
+	return c.Buffer[c.Tail : c.Head+len(c.Buffer)/2]
 }
 
 func (c *Circular) RemainingSpace() int {
-	return (len(c.Buf) / 2) - (c.Tail - c.Head)
+	return (len(c.Buffer) / 2) - (c.Tail - c.Head)
 }
 
 func (c *Circular) Reset() {
@@ -85,14 +85,14 @@ func (c *Circular) UnconsumedLen() int {
 }
 
 func (c *Circular) UnconsumedSlice() []byte {
-	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: uintptr(unsafe.Pointer(&c.Buf[c.Head])), Len: c.UnconsumedLen(), Cap: c.UnconsumedLen()}))
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{Data: uintptr(unsafe.Pointer(&c.Buffer[c.Head])), Len: c.UnconsumedLen(), Cap: c.UnconsumedLen()}))
 }
 
 func (c *Circular) UnconsumedString() string {
-	return *(*string)(unsafe.Pointer(&reflect.StringHeader{Data: uintptr(unsafe.Pointer(&c.Buf[c.Head])), Len: c.UnconsumedLen()}))
+	return *(*string)(unsafe.Pointer(&reflect.StringHeader{Data: uintptr(unsafe.Pointer(&c.Buffer[c.Head])), Len: c.UnconsumedLen()}))
 }
 
 func (c *Circular) Free() {
-	syscall.Munmap(unsafe.Pointer(&c.Buf[0]), uint64(len(c.Buf)))
-	c.Buf = nil
+	syscall.Munmap(unsafe.Pointer(&c.Buffer[0]), uint64(len(c.Buffer)))
+	c.Buffer = nil
 }
