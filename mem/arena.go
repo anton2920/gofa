14353e6d196ddc13895cd8ac2b7ecc9e839acd05
anton2920 <anton2920@gmail.com>
2026-06-20 21:02:59 +0300

    mem: changed Arena implementation to return 'unsafe.Pointer' on allocation

--- mem/arena.go
+++ mem/arena.go
@@ -16,7 +16,7 @@ type Arena struct {
 	CurrOfft int
 }
 
-type ArenaSave struct {
+type ArenaSavePoint struct {
 	Arena    *Arena
 	PrevOfft int
 	CurrOfft int
@@ -24,28 +24,46 @@ type ArenaSave struct {
 
 const arenaDefaultAlignment = unsafe.Alignof(uintptr(0))
 
-func NewArena(buffer []byte) Arena {
-	return Arena{Buffer: buffer}
+func ArenaFromByteSlice(buf []byte) Arena {
+	return Arena{Buffer: bytes.SliceFromBytePointer(&buf[0], len(buf))}
 }
 
-func NewArenaFromBytePointer(ptr *byte, n int) Arena {
-	return NewArena(bytes.SliceFromBytePointer(ptr, n))
+func ArenaFromBytePointer(ptr *byte, n int) Arena {
+	return ArenaFromByteSlice(bytes.SliceFromBytePointer(ptr, n))
 }
 
-func NewArenaFromUnsafePointer(ptr unsafe.Pointer, n int) Arena {
-	return NewArena(bytes.SliceFromUnsafePointer(ptr, n))
+func ArenaFromUnsafePointer(ptr unsafe.Pointer, n int) Arena {
+	return ArenaFromByteSlice(bytes.SliceFromUnsafePointer(ptr, n))
 }
 
-func (a *Arena) Allocate(n int) []byte {
-	return a.AllocateWithAlignment(n, arenaDefaultAlignment)
+func (a *Arena) InitWithByteSlice(buf []byte) {
+	a.Buffer = bytes.SliceFromBytePointer(&buf[0], len(buf))
+	a.PrevOfft = 0
+	a.CurrOfft = 0
 }
 
-func (a *Arena) AllocateWithAlignment(n int, align uintptr) []byte {
+func (a *Arena) InitWithBytePointer(ptr *byte, n int) {
+	a.Buffer = bytes.SliceFromBytePointer(ptr, n)
+	a.PrevOfft = 0
+	a.CurrOfft = 0
+}
+
+func (a *Arena) InitWithUnsafePointer(ptr unsafe.Pointer, n int) {
+	a.Buffer = bytes.SliceFromUnsafePointer(ptr, n)
+	a.PrevOfft = 0
+	a.CurrOfft = 0
+}
+
+func (a *Arena) PushSize(n int) unsafe.Pointer {
+	return a.PushSizeWithAlignment(n, arenaDefaultAlignment)
+}
+
+func (a *Arena) PushSizeWithAlignment(n int, align uintptr) unsafe.Pointer {
 	t := trace.Begin("")
 
 	a.CurrOfft += pointers.Delta(pointers.AlignUp(unsafe.Pointer(&a.Buffer[a.CurrOfft]), align), unsafe.Pointer(&a.Buffer[a.CurrOfft]))
-	if a.CurrOfft+n <= cap(a.Buffer) {
-		debug.AssertZero(int(uintptr(unsafe.Pointer(&a.Buffer[a.CurrOfft]))&(align-1)), "(*Arena).AllocateWithAlignment tried to return unaligned pointer")
+	if a.CurrOfft+n <= len(a.Buffer) {
+		debug.AssertZero(int(uintptr(unsafe.Pointer(&a.Buffer[a.CurrOfft]))&(align-1)), "(*Arena).PushSizeWithAlignment tried to return unaligned pointer")
 		a.PrevOfft = a.CurrOfft
 		a.CurrOfft += n
 		buf := a.Buffer[a.PrevOfft:a.CurrOfft]
@@ -55,42 +73,38 @@ func (a *Arena) AllocateWithAlignment(n int, align uintptr) []byte {
 		}
 
 		trace.End(t)
-		return buf
+		return pointers.UnsafeNoescape(unsafe.Pointer(&buf[0]))
 	}
 
-	debug.Printf("no more space in arena %p: requested %d, got only %d left", &a.Buffer[0], n, cap(a.Buffer)-a.CurrOfft)
+	debug.Printf("no more space in arena %p: requested %d, got only %d left", &a.Buffer[0], n, len(a.Buffer)-a.CurrOfft)
 	trace.End(t)
 	return nil
 }
 
-func (a *Arena) Begin() ArenaSave {
-	return ArenaSave{Arena: a, PrevOfft: a.PrevOfft, CurrOfft: a.CurrOfft}
+func (a *Arena) RepushSize(optr unsafe.Pointer, on int, nn int) unsafe.Pointer {
+	return a.RepushSizeWithAlignment(optr, on, nn, arenaDefaultAlignment)
 }
 
-func (a *Arena) Reallocate(old []byte, n int) []byte {
-	return a.ReallocateWithAlignment(old, n, arenaDefaultAlignment)
-}
-
-func (a *Arena) ReallocateWithAlignment(old []byte, n int, align uintptr) []byte {
+func (a *Arena) RepushSizeWithAlignment(optr unsafe.Pointer, on int, nn int, align uintptr) unsafe.Pointer {
 	t := trace.Begin("")
 
-	if len(old) == 0 {
-		buf := a.AllocateWithAlignment(n, align)
+	if on == 0 {
+		nptr := a.PushSizeWithAlignment(nn, align)
 		trace.End(t)
-		return buf
-	} else if (uintptr(unsafe.Pointer(&old[0])) >= uintptr(unsafe.Pointer(&a.Buffer[0]))) && (uintptr(unsafe.Pointer(&old[len(old)-1])) <= uintptr(unsafe.Pointer(&a.Buffer[a.CurrOfft-1]))) {
-		if &old[0] == &a.Buffer[a.PrevOfft] {
-			if n > len(old) {
-				for i := len(old); i < n; i++ {
+		return nptr
+	} else if (uintptr(optr) >= uintptr(unsafe.Pointer(&a.Buffer[0]))) && ((uintptr(optr) + uintptr(on)) <= uintptr(unsafe.Pointer(&a.Buffer[a.CurrOfft-1]))) {
+		if optr == unsafe.Pointer(&a.Buffer[a.PrevOfft]) {
+			if nn > on {
+				for i := on; i < nn; i++ {
 					a.Buffer[a.PrevOfft+i] = 0
 				}
 			}
-			a.CurrOfft = a.PrevOfft + n
-			return a.Buffer[a.PrevOfft:a.CurrOfft]
+			a.CurrOfft = a.PrevOfft + nn
+			return optr
 		} else {
-			buf := a.AllocateWithAlignment(n, align)
-			copy(buf, old)
-			return buf
+			nptr := a.PushSizeWithAlignment(nn, align)
+			copy(bytes.SliceFromUnsafePointer(nptr, nn), bytes.SliceFromUnsafePointer(optr, on))
+			return nptr
 		}
 	}
 
@@ -99,13 +113,19 @@ func (a *Arena) ReallocateWithAlignment(old []byte, n int, align uintptr) []byte
 }
 
 func (a *Arena) PushInt() *int {
-	buf := a.AllocateWithAlignment(int(unsafe.Sizeof(int(0))), unsafe.Alignof(int(0)))
-	return (*int)(unsafe.Pointer(&buf[0]))
+	return (*int)(a.PushSizeWithAlignment(int(unsafe.Sizeof(int(0))), unsafe.Alignof(int(0))))
 }
 
 func (a *Arena) PushFloat32() *float32 {
-	buf := a.AllocateWithAlignment(int(unsafe.Sizeof(float32(0))), unsafe.Alignof(float32(0)))
-	return (*float32)(unsafe.Pointer(&buf[0]))
+	return (*float32)(a.PushSizeWithAlignment(int(unsafe.Sizeof(float32(0))), unsafe.Alignof(float32(0))))
+}
+
+func (a *Arena) PushByteArray(n int) []byte {
+	return bytes.SliceFromUnsafePointer(a.PushSizeWithAlignment(n, unsafe.Alignof(byte(0))), n)
+}
+
+func (a *Arena) RepushByteArray(old []byte, n int) []byte {
+	return bytes.SliceFromUnsafePointer(a.RepushSizeWithAlignment(unsafe.Pointer(&old[0]), len(old), n, unsafe.Alignof(byte(0))), n)
 }
 
 func (a *Arena) Reset() {
@@ -113,7 +133,11 @@ func (a *Arena) Reset() {
 	a.CurrOfft = 0
 }
 
-func (a *ArenaSave) Rollback() {
+func (a *Arena) Begin() ArenaSavePoint {
+	return ArenaSavePoint{Arena: a, PrevOfft: a.PrevOfft, CurrOfft: a.CurrOfft}
+}
+
+func (a *ArenaSavePoint) Rollback() {
 	a.Arena.PrevOfft = a.PrevOfft
 	a.Arena.CurrOfft = a.CurrOfft
 }
