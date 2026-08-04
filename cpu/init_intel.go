e07dcc07648b7435686aa05fa13c1d0289245d86
anton2920 <anton2920@gmail.com>
2026-08-04 18:13:06 +0300

    cpu: removed usage of 'debug_'

--- cpu/init_intel.go
+++ cpu/init_intel.go
@@ -3,16 +3,10 @@
 
 package cpu
 
-import (
-	"github.com/anton2920/gofa/cpu/intel"
-	"github.com/anton2920/gofa/debug_"
-)
+import "github.com/anton2920/gofa/cpu/intel"
 
 var CPUHz Cycles
 
 func init() {
 	CPUHz = Cycles(intel.CPUHz)
-	if CPUHz != 0 {
-		debug_.Printf("[cpu]: CPU Frequency %dHz", CPUHz)
-	}
 }
