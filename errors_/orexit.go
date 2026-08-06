fc18f9bf20a56b450cb344fc1e9c095fd548ed60
anton2920 <anton2920@gmail.com>
2026-08-06 15:48:27 +0300

    errors_: added new package with convenience function 'OrExit'

---
+++ errors_/orexit.go
@@ -0,0 +1,17 @@
+package errors_
+
+import (
+	"github.com/anton2920/gofa/fmt"
+	"github.com/anton2920/gofa/fmt_"
+	"github.com/anton2920/gofa/os"
+)
+
+func OrExit(msg string, ierr error) {
+	if ierr != nil {
+		var f fmt.Formatter
+		f.InitWithByteSlice(make([]byte, 4096))
+
+		fmt_.Eprintln(f.S("ERROR: ").S(msg).S(ierr.Error()))
+		os.Exit(1)
+	}
+}
