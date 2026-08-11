281fd6941ed5d742fbd321967fb7670d8715c63c
anton2920 <anton2920@gmail.com>
2026-08-11 15:58:13 +0300

    strings: added 'Trim' function from standard library

--- strings/trim.go
+++ strings/trim.go
@@ -2,6 +2,10 @@ package strings
 
 import "strings"
 
+func Trim(s string, cutset string) string {
+	return strings.Trim(s, cutset)
+}
+
 func TrimSpace(s string) string {
 	return strings.TrimSpace(s)
 }
