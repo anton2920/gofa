34044ac43699a839da91a6d23d9d81cf89cddc3c
anton2920 <anton2920@gmail.com>
2026-08-13 14:59:06 +0300

    context: removed 'MinimumLogLevel' from init methods

--- context/context.go
+++ context/context.go
@@ -1,9 +1,12 @@
 package context
 
 import (
+	"unsafe"
+
 	"github.com/anton2920/gofa/fmt"
 	"github.com/anton2920/gofa/log"
 	"github.com/anton2920/gofa/mem"
+	"github.com/anton2920/gofa/pointers"
 )
 
 type Context struct {
@@ -17,11 +20,11 @@ type Context struct {
 	ErrCurr int
 }
 
-func (ctx *Context) InitWithMinimumLogLevelAndFourByteSlices(level log.Level, arenaBuf []byte, fmtBuf []byte, logBuf []byte, errBuf []byte) {
+func (ctx *Context) InitWithFourByteSlices(arenaBuf []byte, fmtBuf []byte, logBuf []byte, errBuf []byte) {
 	ctx.Arena.InitWithByteSlice(arenaBuf)
 
 	ctx.Fmt.InitWithByteSlice(fmtBuf)
-	ctx.Log.InitWithMinimumLevelAndByteSlice(level, logBuf)
+	ctx.Log.InitWithByteSlice(logBuf)
 
 	size := len(errBuf) / len(ctx.ErrFmt)
 	for i := 0; i < len(ctx.ErrFmt); i++ {
@@ -29,13 +32,9 @@ func (ctx *Context) InitWithMinimumLogLevelAndFourByteSlices(level log.Level, ar
 	}
 }
 
-func (ctx *Context) InitWithMinimumLogLevelAndEvenlySplitByteSlice(level log.Level, buf []byte) {
+func (ctx *Context) InitWithEvenlySplitByteSlice(buf []byte) {
 	size := len(buf) / 4
-	ctx.InitWithMinimumLogLevelAndFourByteSlices(level, buf[0:size], buf[size:2*size], buf[2*size:3*size], buf[3*size:])
-}
-
-func (ctx *Context) Init(level log.Level, buf []byte) {
-	ctx.InitWithMinimumLogLevelAndEvenlySplitByteSlice(level, buf)
+	ctx.InitWithFourByteSlices(buf[0:size], buf[size:2*size], buf[2*size:3*size], buf[3*size:])
 }
 
 func (ctx *Context) Error() string {
@@ -46,11 +45,11 @@ func (ctx *Context) ErrorCode() int {
 	return ctx.ErrCode[ctx.ErrCurr]
 }
 
-func (ctx *Context) LastError() string {
+func (ctx *Context) OldError() string {
 	return ctx.ErrFmt[1-ctx.ErrCurr].String()
 }
 
-func (ctx *Context) LastErrorCode() int {
+func (ctx *Context) OldErrorCode() int {
 	return ctx.ErrCode[1-ctx.ErrCurr]
 }
 
@@ -62,4 +61,8 @@ func (ctx *Context) NewErrorWithCode(code int) *fmt.Formatter {
 
 func (ctx *Context) NewError() *fmt.Formatter {
 	return ctx.NewErrorWithCode(0)
+}
+
+func (ctx *Context) Noescape() *Context {
+	return (*Context)(pointers.UnsafeNoescape(unsafe.Pointer(ctx)))
 }
