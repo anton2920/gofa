b70e86a5d365965d6e0c9b81b06ba5bb063ed5e1
anton2920 <anton2920@gmail.com>
2026-08-18 20:52:50 +0300

    container/binary: removed node allocation stuff

--- container/binary/tree.go
+++ container/binary/tree.go
@@ -1,12 +1,6 @@
 package binary
 
-import (
-	"unsafe"
-
-	"github.com/anton2920/gofa/mem"
-)
-
-type TreeKeyComparator func(*TreeNode, *TreeNode) int
+type TreeNodeComparator func(*TreeNode, *TreeNode) int
 
 type TreeNode struct {
 	Left  *TreeNode
@@ -17,52 +11,86 @@ type SearchTree struct {
 	N int
 
 	Root *TreeNode
-
-	Arena             *mem.Arena
-	ElementSize       uintptr
-	ElementAlignment  uintptr
-	ElementComparator TreeKeyComparator
 }
 
-func ArenaPushTreeNode(a *mem.Arena, elementSize uintptr, elementAlignment uintptr) *TreeNode {
-	alignment := unsafe.Alignof(TreeNode{})
-	if elementAlignment > alignment {
-		alignment = elementAlignment
-	}
-	return (*TreeNode)(a.PushSizeWithAlignment(unsafe.Sizeof(TreeNode{})+elementSize, alignment))
-}
-
-func (bt *SearchTree) Init(a *mem.Arena, elementSize uintptr, elementAlignment uintptr, cmp TreeKeyComparator) {
-	bt.Arena = a
-	bt.ElementSize = elementSize
-	bt.ElementAlignment = elementAlignment
-	bt.ElementComparator = cmp
-}
-
-func (bt *SearchTree) NewNode() *TreeNode {
-	return ArenaPushTreeNode(bt.Arena, bt.ElementSize, bt.ElementAlignment)
-}
-
-func (bt *SearchTree) insertAsAChildOf(n *TreeNode, parent **TreeNode) {
-	if *parent == nil {
-		*parent = n
+func (bt *SearchTree) addAsAChildOf(key *TreeNode, pnode **TreeNode, cmp TreeNodeComparator) {
+	if *pnode == nil {
+		*pnode = key
 		return
 	}
+	node := *pnode
 
-	p := *parent
-	cmp := bt.ElementComparator(p, n)
-	if cmp <= 0 {
-		bt.insertAsAChildOf(n, &p.Left)
-	} else {
-		bt.insertAsAChildOf(n, &p.Right)
+	res := cmp(key, node)
+	switch {
+	case res < 0:
+		bt.addAsAChildOf(key, &node.Left, cmp)
+	case res > 0:
+		bt.addAsAChildOf(key, &node.Right, cmp)
 	}
 }
 
-func (bt *SearchTree) Insert(n *TreeNode) {
-	if bt.Root == nil {
-		bt.Root = n
-	} else {
-		bt.insertAsAChildOf(n, &bt.Root)
-	}
+func (bt *SearchTree) Add(key *TreeNode, cmp TreeNodeComparator) {
+	bt.addAsAChildOf(key, &bt.Root, cmp)
 	bt.N++
+}
+
+func (bt *SearchTree) getFromChildOf(key *TreeNode, node *TreeNode, cmp TreeNodeComparator) *TreeNode {
+	if node == nil {
+		return nil
+	}
+
+	res := cmp(key, node)
+	switch {
+	case res < 0:
+		return bt.getFromChildOf(key, node.Left, cmp)
+	case res > 0:
+		return bt.getFromChildOf(key, node.Right, cmp)
+	}
+
+	return node
+}
+
+func (bt *SearchTree) Get(key *TreeNode, cmp TreeNodeComparator) *TreeNode {
+	return bt.getFromChildOf(key, bt.Root, cmp)
+}
+
+func (bt *SearchTree) Has(key *TreeNode, cmp TreeNodeComparator) bool {
+	return bt.Get(key, cmp) != nil
+}
+
+func (bt *SearchTree) delFromParent(key *TreeNode, node *TreeNode, parent *TreeNode, cmp TreeNodeComparator) *TreeNode {
+	if node == nil {
+		return nil
+	}
+
+	res := cmp(key, node)
+	switch {
+	case res < 0:
+		return bt.delFromParent(key, node.Left, node, cmp)
+	case res > 0:
+		return bt.delFromParent(key, node.Right, node, cmp)
+	}
+
+	if parent.Left == node {
+		parent.Left = node.Left
+		if node.Right != nil {
+			bt.addAsAChildOf(node.Right, &parent.Right, cmp)
+		}
+	} else {
+		parent.Right = node.Right
+		if node.Left != nil {
+			bt.addAsAChildOf(node.Left, &parent.Left, cmp)
+		}
+	}
+
+	node.Left, node.Right = nil, nil
+	return node
+}
+
+func (bt *SearchTree) Del(key *TreeNode, cmp TreeNodeComparator) *TreeNode {
+	n := bt.delFromParent(key, bt.Root, nil, cmp)
+	if n != nil {
+		bt.N--
+	}
+	return n
 }
