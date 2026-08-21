4b7ccf4dff404fd9b1d80f7fd5bd16f9cae2532c
anton2920 <anton2920@gmail.com>
2026-08-21 16:19:10 +0300

    container/binary: changed type of Comparator function and added proper delete method

--- container/binary/tree.go
+++ container/binary/tree.go
@@ -1,96 +1,120 @@
 package binary
 
-type TreeNodeComparator func(*TreeNode, *TreeNode) int
+import (
+	"unsafe"
+
+	"github.com/anton2920/gofa/pointers"
+)
+
+type (
+	TreeKeyPointer    unsafe.Pointer
+	TreeKeyComparator func(TreeKeyPointer, TreeKeyPointer) int
+)
 
 type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+func (n *TreeNode) KeyPointer() TreeKeyPointer {
+	return (TreeKeyPointer)(pointers.Add(unsafe.Pointer(n), unsafe.Sizeof(*n)))
+}
+
 type SearchTree struct {
 	N int
 
 	Root *TreeNode
+
+	Comparator TreeKeyComparator
 }
 
-func (bt *SearchTree) addAsAChildOf(key *TreeNode, pnode **TreeNode, cmp TreeNodeComparator) {
-	if *pnode == nil {
-		*pnode = key
+func (bt *SearchTree) addAsAChildOf(node *TreeNode, pparent **TreeNode) {
+	if *pparent == nil {
+		*pparent = node
+		bt.N++
 		return
 	}
-	node := *pnode
+	parent := *pparent
 
-	res := cmp(key, node)
+	res := bt.Comparator(node.KeyPointer(), parent.KeyPointer())
 	switch {
 	case res < 0:
-		bt.addAsAChildOf(key, &node.Left, cmp)
+		bt.addAsAChildOf(node, &parent.Left)
 	case res > 0:
-		bt.addAsAChildOf(key, &node.Right, cmp)
+		bt.addAsAChildOf(node, &parent.Right)
 	}
 }
 
-func (bt *SearchTree) Add(key *TreeNode, cmp TreeNodeComparator) {
-	bt.addAsAChildOf(key, &bt.Root, cmp)
-	bt.N++
+func (bt *SearchTree) Add(node *TreeNode) {
+	bt.addAsAChildOf(node, &bt.Root)
 }
 
-func (bt *SearchTree) getFromChildOf(key *TreeNode, node *TreeNode, cmp TreeNodeComparator) *TreeNode {
+func (bt *SearchTree) getFromChildOf(key TreeKeyPointer, node *TreeNode) *TreeNode {
 	if node == nil {
 		return nil
 	}
 
-	res := cmp(key, node)
+	res := bt.Comparator(key, node.KeyPointer())
 	switch {
 	case res < 0:
-		return bt.getFromChildOf(key, node.Left, cmp)
+		return bt.getFromChildOf(key, node.Left)
 	case res > 0:
-		return bt.getFromChildOf(key, node.Right, cmp)
+		return bt.getFromChildOf(key, node.Right)
 	}
 
 	return node
 }
 
-func (bt *SearchTree) Get(key *TreeNode, cmp TreeNodeComparator) *TreeNode {
-	return bt.getFromChildOf(key, bt.Root, cmp)
+func (bt *SearchTree) Get(key TreeKeyPointer) *TreeNode {
+	return bt.getFromChildOf(key, bt.Root)
 }
 
-func (bt *SearchTree) Has(key *TreeNode, cmp TreeNodeComparator) bool {
-	return bt.Get(key, cmp) != nil
+func (bt *SearchTree) Has(key TreeKeyPointer) bool {
+	return bt.Get(key) != nil
 }
 
-func (bt *SearchTree) delFromParent(key *TreeNode, node *TreeNode, parent *TreeNode, cmp TreeNodeComparator) *TreeNode {
-	if node == nil {
+func (bt *SearchTree) delFromChildOf(key TreeKeyPointer, pnode **TreeNode) *TreeNode {
+	if *pnode == nil {
 		return nil
 	}
+	node := *pnode
 
-	res := cmp(key, node)
+	res := bt.Comparator(key, node.KeyPointer())
 	switch {
 	case res < 0:
-		return bt.delFromParent(key, node.Left, node, cmp)
+		return bt.del(key, &node.Left)
 	case res > 0:
-		return bt.delFromParent(key, node.Right, node, cmp)
+		return bt.del(key, &node.Right)
 	}
 
-	if parent.Left == node {
-		parent.Left = node.Left
-		if node.Right != nil {
-			bt.addAsAChildOf(node.Right, &parent.Right, cmp)
-		}
+	if node.Left == nil {
+		*pnode = node.Right
+	} else if node.Right == nil {
+		*pnode = node.Left
 	} else {
-		parent.Right = node.Right
-		if node.Left != nil {
-			bt.addAsAChildOf(node.Left, &parent.Left, cmp)
+		parent := node
+		it := parent.Left
+		for it.Right != nil {
+			parent = it
+			it = it.Right
 		}
+
+		if parent == node {
+			parent.Left = it.Left
+		} else {
+			parent.Right = it.Left
+		}
+
+		it.Left = node.Left
+		it.Right = node.Right
+		*pnode = it
 	}
 
+	bt.N--
 	node.Left, node.Right = nil, nil
 	return node
 }
 
-func (bt *SearchTree) Del(key *TreeNode, cmp TreeNodeComparator) *TreeNode {
-	n := bt.delFromParent(key, bt.Root, nil, cmp)
-	if n != nil {
-		bt.N--
-	}
-	return n
+func (bt *SearchTree) Del(key TreeKeyPointer) *TreeNode {
+	return bt.delFromChildOf(key, &bt.Root)
 }
