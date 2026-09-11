820c4630810bbfa50501ada88e67300a271514ff
anton2920 <anton2920@gmail.com>
2026-09-12 01:33:39 +0300

    cpu: added 'CacheLineSize' constant

---
+++ cpu/cache_intel.go
@@ -0,0 +1,8 @@
+//go:build 386 || amd64
+// +build 386 amd64
+
+package cpu
+
+import "github.com/anton2920/gofa/cpu/intel"
+
+const CacheLineSize = intel.CacheLineSize
---
+++ cpu/intel/cache.go
@@ -0,0 +1,3 @@
+package intel
+
+const CacheLineSize = 64
