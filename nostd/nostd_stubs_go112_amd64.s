b90a74b6f29a827ed81cb6d8c6562c34acfd0521
anton2920 <anton2920@gmail.com>
2026-09-22 21:17:22 +0300

    nostd: updated stubs for Go 1.15

--- nostd/nostd_stubs_go112_amd64.s
+++ nostd/nostd_stubs_go112_amd64.s
@@ -1,6 +1,21 @@
 //go:build: amd64 && gofanostd && gofanostd112
 // +build amd64,gofanostd,gofanostd112
 
+// gcWriteBarrierBX is gcWriteBarrier, but with args in DI and BX.
+TEXT runtime·gcWriteBarrierBX(SB), 6, $-0
+	XCHGQ BX, AX
+	CALL runtime·gcWriteBarrier(SB)
+	XCHGQ BX, AX
+	RET
+
+// gcWriteBarrierR9 is gcWriteBarrier, but with args in DI and R9.
+TEXT runtime·gcWriteBarrierR9(SB), 6, $-0
+	XCHGQ R9, AX
+	CALL runtime·gcWriteBarrier(SB)
+	XCHGQ R9, AX
+	RET
+
+
 TEXT runtime·duffzero(SB), 6, $-0
 	MOVUPS	X0,(DI)
 	MOVUPS	X0,16(DI)
